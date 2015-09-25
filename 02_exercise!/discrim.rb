7638ba43e40623f4098bfca20e6cb79c54522407
Aleksey Kubarev <aleksey.kubarev@gmail.com>
2015-09-26 00:11:57 +0300

    <Вычисляем дискриминант>

--- 02_exercise!/discrim.rb
+++ 02_exercise!/discrim.rb
@@ -5,7 +5,7 @@ b = gets.chomp.to_f
 print "Введите коэффициент c вашего квадратного уравнения: "
 c = gets.chomp.to_f
 
-d = b**2 - (4*a*c)
+d = b**2 - 4*a*c
 
 if d > 0
 	x1 = -b + Math.sqrt(d)/2*a
